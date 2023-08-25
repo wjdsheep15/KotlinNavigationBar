6c627a9192c62088d38fe560270a63bdc7e4c828
JungSeungGeun <wjd15sheep@naver.com>
2023-08-25 13:16:49 +0900

    BottomNavigation 구성 연습

---
+++ app/src/main/java/com/example/navigationpratice/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.navigationpratice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.navigationpratice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.nabBottom, navController)
+    }
+}
